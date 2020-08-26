243cf7fb0ad4f5e533b6bd5ad6600ecb3a2d6c5e
Xiao_Jin <34389622+jinzhijie@users.noreply.github.com>
2020-08-25 19:56:22 -0500

    Fix 'set' object has no attribute 'strip'

--- csllogparser.py
+++ csllogparser.py
@@ -129,5 +129,5 @@ def aoscPastebin(url: str, fromLittleSkin: bool = True) -> str:
     rawUrl = f'{url}/raw'
     r = requests.get(rawUrl)
     m1, m2 = cslHandler(r.text, fromLittleSkin=fromLittleSkin)
-    m3 = str().join(m2.strip('\n'))
-    return f'{m1}{m3}'
+    m3 = str().join(m2)
+    return f'{m1}{m3}'.strip('\n')
