207d882b895b65d69e82c74a28fa6b1d8d52b0a9
bingling-sama <65523047+bingling-sama@users.noreply.github.com>
2020-09-06 09:15:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ async def anti_bad_words(app: GraiaMiraiApplication, group: Group):
 
 
 @bcc.receiver(GroupMessage, headless_decoraters=[Depend(onMatch(r'^草*$')),
-                                                 Depend(inGroups([qq.honoka_cafe]))])
+                                                 Depend(exceptGroups([qq.honoka_cafe,qq.csl_group]))])
 async def grass_spammer(app: GraiaMiraiApplication, group: Group):
     await app.sendGroupMessage(group, MessageChain.create([Plain('草\u202e')]))
 
