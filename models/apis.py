21643f75b56444a80bfb887f69ab9948e8d9c9f5
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2021-02-15 18:23:50 +0800

    Add several apis (with pydantic) - CustomSkinLoader Download & Player Info - Authlib-Injector Download - Yggdrasil (third-party) Player Info

--- models/apis.py
+++ models/apis.py
@@ -1,41 +1,126 @@
-from typing import Literal, Optional, Any
-from pydantic import BaseModel
+import json
+from base64 import b64decode
+from datetime import date
+from typing import Literal, Optional
 
-
-class CustomSkinLoaderLatestDownloads(BaseModel):
-    Fabric: str
-    Forge: str
+import aiohttp
+from pydantic import BaseModel, root_validator, validator
 
 
 class CustomSkinLoaderLatest(BaseModel):
+    class Downloads(BaseModel):
+        Fabric: str
+        Forge: str
     version: str
-    downloads: CustomSkinLoaderLatestDownloads
+    downloads: Downloads
 
-
-class AuthlibInjectorLatestCheckSums(BaseModel):
-    sha256: str
+    @classmethod
+    async def get(cls):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://csl-1258131272.cos.ap-shanghai.myqcloud.com/latest.json') as resp:
+                return cls.parse_raw(await resp.text())
 
 
 class AuthlibInjectorLatest(BaseModel):
+    class CheckSums(BaseModel):
+        sha256: str
     build_number: int
     version: str
     download_url: str
-    checksums: AuthlibInjectorLatestCheckSums
+    checksums: CheckSums
 
-
-class CustomSkinLoaderApiSkins(BaseModel):
-    slim: Optional[str]
-    default: Optional[str]
+    @classmethod
+    async def get(cls):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://authlib-injector.yushi.moe/artifact/latest.json') as resp:
+                return cls.parse_raw(await resp.text())
 
 
 class CustomSkinLoaderApi(BaseModel):
+    class Skins(BaseModel):
+        slim: Optional[str]
+        default: Optional[str]
+
     username: Optional[str]
-    skins: Optional[CustomSkinLoaderApiSkins]
+    skins: Optional[Skins]
     cape: Optional[str]
     existed: bool = True
-    skin_type: Literal['default', 'slim']
+    skin_type: Optional[Literal['default', 'slim']] = 'default'
 
-    def __init__(__pydantic_self__, **data: Any) -> None:
-        super().__init__(**data)
-        __pydantic_self__.existed = bool(__pydantic_self__.username)
-        __pydantic_self__.skins = 'default' if __pydantic_self__.skins.default else 'slim'
+    @root_validator(pre=True)
+    def extra_info(cls, values: dict):
+        existed = 'username' in values and values['username'] != '404'
+        if not existed or 'skins' not in values:
+            skin_type = None
+        else:
+            skin_type = 'default' if 'default' not in values['skins'] else 'slim'
+        values.update({
+            'existed': existed,
+            'skin_type': skin_type
+        })
+        return values
+
+    @classmethod
+    async def get(cls, api_root: str, username: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{api_root}/{username}.json') as resp:
+                return cls.parse_raw(await resp.text())
+
+
+class YggdrasilPlayerUuidApi(BaseModel):
+    id: Optional[str] = None
+    name: Optional[str] = None
+    existed: bool = True
+
+    @classmethod
+    async def get(cls, api_root: str, username: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{api_root}/api/users/profiles/minecraft/{username}') as resp:
+                if resp.status == 204:  # No content
+                    return cls(existed=False)
+                return cls.parse_raw(await resp.text())
+
+
+class YggdrasilTextures(BaseModel):
+    class Skin(BaseModel):
+        class MetaData(BaseModel):
+            model: Literal['default', 'slim']
+        url: str
+        metadata: Optional[MetaData]
+
+    class Cape(BaseModel):
+        url: str
+
+    SKIN: Skin
+    CAPE: Cape
+
+
+class YggdrasilPropertiesTextures(BaseModel):
+    timestamp: date
+    profileId: str
+    profileName: str
+    textures: YggdrasilTextures
+
+
+class YggdrasilGameProfileApi(BaseModel):
+    class Properties(BaseModel):
+        textures: YggdrasilPropertiesTextures
+    id: str
+    name: str
+    properties: Properties  # a bit difference between API
+
+    @root_validator(pre=True)
+    def pre_process(cls, values):
+        # Doc: https://wiki.vg/Mojang_API#UUID_-.3E_Profile_.2B_Skin.2FCape
+        # base64 decode and a little change
+        values['properties'][0]['textures'] = json.loads(b64decode(
+            values['properties'][0]['value']))
+        # array is useless while mojang is interesting
+        values['properties'] = values['properties'][0]
+        return values
+
+    @classmethod
+    async def get(cls, api_root: str, player_uuid: str):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{api_root}/sessionserver/session/minecraft/profile/{player_uuid}') as resp:
+                return cls.parse_raw(await resp.text())
