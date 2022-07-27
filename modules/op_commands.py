6bbfa7e31e4ad09c0d13166af67414acd95cb50e
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2022-07-27 11:52:28 +0800

    Remove custom time of mute

--- modules/op_commands.py
+++ modules/op_commands.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Tuple
 
 from graia.ariadne.app import Ariadne
 from graia.ariadne.message.chain import MessageChain
@@ -6,10 +6,7 @@ from graia.ariadne.message.commander.saya import CommandSchema
 from graia.ariadne.message.element import At, Plain, Quote, Source
 from graia.ariadne.model import Group, Member
 from graia.broadcast.builtin.decorators import Depend
-from graia.broadcast.exceptions import ExecutionStop
 from graia.saya import Channel
-from settings import specialqq as qq
-from texts import TextFields as tF
 from utils.permissons import require_admin
 
 channel = Channel.current()
@@ -25,11 +22,11 @@ async def recall(app: Ariadne, message: MessageChain):
         await app.recall_message(current_message)
 
 
-@channel.use(CommandSchema('&mute {time: int = 10} {...targets: At}', decorators=[Depend(require_admin)]))
-async def mute(app: Ariadne, group: Group, time: int, targets: Tuple[At]):
+@channel.use(CommandSchema('&mute {...targets: At}', decorators=[Depend(require_admin)]))
+async def mute(app: Ariadne, group: Group, targets: Tuple[At]):
     targets = [m.target for m in targets]
     for target in targets:
-        await app.mute_member(group, target, time * 60)
+        await app.mute_member(group, target, 10 * 60)
 
 
 @channel.use(CommandSchema('&unmute {...targets: At}', decorators=[Depend(require_admin)]))
