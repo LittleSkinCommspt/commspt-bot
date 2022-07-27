51968f026039f528c9f8391fa4c1268e5907d1f0
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2022-07-27 11:52:49 +0800

    Remove Comments

--- main.py
+++ main.py
@@ -20,33 +20,5 @@ with saya.module_context():
         saya.require(f'modules.{module_info.name}')
 
 
-
-
-# @broadcast.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([CommandMatch('view')], {"params": WildcardMatch(optional=True)}))])
-# async def command_handler(app: Ariadne, group: Group, params: WildcardMatch):
-#     player_name = params.result.asDisplay()
-#     result = await apis.CustomSkinLoaderApi.get('https://littleskin.cn/csl', player_name)
-#     if not result.player_existed:
-#         await app.sendGroupMessage(group, MessageChain.create([Plain(f'「{player_name}」不存在')]))
-#     else:
-#         bs_root = 'https://littleskin.cn'
-#         preview_images: List[Image] = list()
-#         for texture in [result.skin_hash, result.cape_hash]:
-#             if texture:
-#                 preview_images.append(Image(data_bytes=await apis.getTexturePreview(
-#                     bs_root, texture)))
-#     await app.sendGroupMessage(group,
-#                                MessageChain.create([*preview_images,
-#                                                     Plain(f'''「{player_name}」
-# Skin: {result.skin_hash[:7]} [{result.skin_type}]
-# Cape: {result.cape_hash[:7] if result.cape_existed else None}''')]))
-
-
-# @broadcast.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([CommandMatch('view.mojang')], {"params": WildcardMatch(optional=True)}))])
-# async def command_handler(app: Ariadne, group: Group, params: WildcardMatch):
-
-
-
-
 if __name__ == '__main__':
     app.launch_blocking()
