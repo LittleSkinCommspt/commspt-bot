bf8f671c7013b3b14b698a70363b0129b8138827
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2023-08-20 23:02:46 +0800

    ✨ feat(check): add check command

--- modules/get_texture.py
+++ modules/get_texture.py
@@ -10,6 +10,7 @@ from graia.saya import Channel
 from models.apis import CustomSkinLoaderApi
 
 from yggdrasil_mc import YggdrasilPlayer
+from yggdrasil_mc.model import YggdrasilTexturesModel
 
 channel = Channel.current()
 
@@ -34,12 +35,84 @@ async def ygg(app: Ariadne, group: Group, player_name: str):
     if not player_uuid.existed:
         _message = f"「{player_name}」不存在"
     else:
-        result = await ygg_server.Profile.get3rdAsync(
-            player_uuid.id
-        )
+        result = await ygg_server.Profile.get3rdAsync(player_uuid.id)
         textures = result.properties.textures.textures
         _message = f"""「{result.name}」
 Skin: {textures.skin.hash[:8] if textures.skin.url else None} [{textures.skin.metadata.model if textures.skin else None}]
 Cape: {textures.cape.hash[:8] if textures.cape.url else None}
 UUID: {UUID(player_uuid.id)}"""
     await app.send_message(group, MessageChain([Plain(_message)]))
+
+
+def is_existed(value: bool) -> str:
+    """Check if a value exists or not.
+
+    Args:
+        value (bool): The value to check.
+
+    Returns:
+        str: "存在" if the value is True, "不存在" otherwise.
+    """
+    return "存在" if value else "不存在"
+
+
+@channel.use(CommandSchema("&check {player_name: str}"))
+async def check(app: Ariadne, group: Group, player_name: str):
+    _message: list[str] = [f"「{player_name}」的报告"]
+
+    csl_texture = await CustomSkinLoaderApi.get(
+        "https://littleskin.cn/csl", player_name
+    )
+
+    ygg_server = YggdrasilPlayer("https://littleskin.cn/api/yggdrasil")
+    ygg_uuid_result = await ygg_server.Uuid.get3rdAsync(player_name)
+
+    if not csl_texture.player_existed == ygg_uuid_result.existed:
+        await app.send_message(
+            group,
+            MessageChain(
+                [
+                    Plain(f"{_message[0]}\n"),
+                    Plain(
+                        f"- 此角色在 CSL API 上{is_existed(ygg_uuid_result.existed)}，在 YGG API 上却{is_existed(csl_texture.player_existed)}！"
+                    ),
+                ]
+            ),
+        )
+        return
+
+    if not ygg_uuid_result.existed and not csl_texture.player_existed:
+        await app.send_message(
+            group,
+            MessageChain(
+                [Plain(f"{_message[0]}\n"), Plain(f"- 此角色在 LittleSkin 中未被找到！")]
+            ),
+        )
+        return
+
+    # check textures
+    ygg_texture = await ygg_server.Profile.get3rdAsync(ygg_uuid_result.id)
+
+    # - skin
+    if csl_texture.skin_hash != ygg_texture.properties.textures.textures.skin.hash:
+        _message.append("- Skin Hash 不一致！")
+
+    # - cape
+    if csl_texture.cape_hash != ygg_texture.properties.textures.textures.cape.hash:
+        _message.append("- Cape Hash 不一致！")
+
+    if len(_message) == 1:
+        _message.append("- 一致性检查通过。")
+    else:
+        _message.append("- 一致性检查失败！")
+    
+    # check mojang
+    ygg_mojang = YggdrasilPlayer()
+    ygg_mojang_uuid_result = await ygg_mojang.Uuid.getMojangAsync(player_name)
+
+    if ygg_mojang_uuid_result.existed:
+        _message.append("- 在 Mojang 中存在同名角色！")
+    else:
+        _message.append("- 在 Mojang 中不存在同名角色。")
+
+    await app.send_message(group, MessageChain([Plain("\n".join(_message))]))
